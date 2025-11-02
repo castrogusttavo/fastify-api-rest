9198537889660e38b4b05930975202021f6c43fa
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2025-11-01 22:04:29 -0300

    second commit

--- src/database.ts
+++ src/database.ts
@@ -4,9 +4,12 @@ import { env } from "./env/index.js";
 
 export const config: Knex.Config = {
   client: "sqlite3",
-  connection: {
-    filename: env.DATABASE_URL || "./db/app.db",
-  },
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
